d692bcee6fc757b07a8f9e0e8afe7b9e606496a4
Shashank Chikattimala <shashankcm90@gmail.com>
2020-10-27 09:02:05 -0500

    Create isValidParentheses.js

---
+++ isValidParentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(str) {
+
+  if (str.length <= 1)
+    return false
+
+  let matchingOpeningBracket, ch
+  let stack = []
+
+  let openingBrackets = ['[', '{', '(']
+  let closingBrackets = [']', '}', ')']
+
+  for (let i = 0; i < str.length; i++) {
+    ch = str[i]
+
+    if (closingBrackets.indexOf(ch) > -1) {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+        return false
+      }
+    } else {
+      stack.push(ch)
+    }
+  }
+
+  return (stack.length == 0)
+};
+
+console.log(isValid("()[]{}"))
+console.log(isValid("(]"))
+console.log(isValid("([)]"))
+console.log(isValid("{[]}"))
+console.log(isValid("([)]"))
